9ef16d8d6cb2d8b634974c6ed7b42a9d9c720e3f
Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-06-05 16:11:37 +0200

    script realizado

--- sql/script_db.sql
+++ sql/script_db.sql
@@ -4,20 +4,20 @@
     @author Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>.
 */
 
---Creo la base de datos aprende inglés
+-- Creo la base de datos aprende inglés
 CREATE DATABASE Aprende_Ingles;
 
---Uso la base de datos creada anteriormente para crear la estructura de esta 
+-- Uso la base de datos creada anteriormente para crear la estructura de esta 
 USE Aprende_Ingles;
 
---Creo la tabla categorías
+-- Creo la tabla categorías
 CREATE TABLE Categorias(
 
     id_categoria tinyint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nombre varchar(30) NOT NULL
 );
 
---Creo la tabla Palabras
+-- Creo la tabla Palabras
 CREATE TABLE Palabras(
 
     id_palabra smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -28,7 +28,7 @@ CREATE TABLE Palabras(
     CONSTRAINT FK_Categoria FOREIGN KEY (categoria) REFERENCES Categorias (id_categoria)
 );
 
---Creo la tabla Usuarios
+-- Creo la tabla Usuarios
 CREATE TABLE Usuarios(
 
     id_usuario smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -36,13 +36,24 @@ CREATE TABLE Usuarios(
     password varchar(25) NOT NULL
 );
 
---Creo la tabla Prac_Ejer
+-- Creo la tabla Prac_Ejer
 CREATE TABLE Prac_Ejer(
 
     id_prac_ejer smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
     fecha_hora datetime NOT NULL,
-    numIntentos tinyint unsigned NOT NULL,
-    superado char(1) NOT NULL,
+    numIntentos tinyint unsigned DEFAULT 0,
+    superado char(1) NOT NULL CHECK (superado IN ('S','N')),
     usuario smallint unsigned NOT NULL,
-    CONSTRAINT FK_Usuario FOREIGN KEY (usuario) REFERENCES Usuarios (id_usuario),
+    CONSTRAINT FK_Usuario FOREIGN KEY (usuario) REFERENCES Usuarios (id_usuario)
+);
+
+-- Creo la tabla Palabras_Ejer
+CREATE TABLE Palabras_Ejer(
+
+    id_palabra smallint unsigned,
+    id_prac_ejer smallint unsigned,
+    fallada char(1) NOT NULL CHECK (fallada IN ('S','N')),
+    PRIMARY KEY (id_palabra, id_prac_ejer),
+    CONSTRAINT FK_Palabra FOREIGN KEY (id_palabra) REFERENCES Palabras (id_palabra),
+    CONSTRAINT FK_Prac_Ejer FOREIGN KEY (id_prac_ejer) REFERENCES Prac_Ejer (id_prac_ejer)
 )
